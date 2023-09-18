add39cf1329d55b4950db9b3dd21bae796665a28
scinkir <scinkir@turksat.com.tr>
2023-09-18 14:54:57 +0300

    first commit

---
+++ src/main/java/tr/com/tsat/staj23eureka/Staj23EurekaApplication.java
@@ -0,0 +1,13 @@
+package tr.com.tsat.staj23eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Staj23EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Staj23EurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/com/tsat/staj23eureka/Staj23EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.tsat.staj23eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Staj23EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
