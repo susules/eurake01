3d67ef34c3a5f73acf425e79dea95cd82bd17c51
scinkir <scinkir@turksat.com.tr>
2023-09-19 11:09:56 +0300

    ..

--- src/main/java/tr/com/tsat/staj23eureka/Staj23EurekaApplication.java
+++ src/main/java/tr/com/tsat/staj23eureka/Staj23EurekaApplication.java
@@ -2,12 +2,14 @@ package tr.com.tsat.staj23eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Staj23EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Staj23EurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Staj23EurekaApplication.class, args);
+    }
 
 }
